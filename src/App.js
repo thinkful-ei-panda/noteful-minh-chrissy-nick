a23462ef464e85ad18c6ccdf6f7763b5942d17b9
nickthorpe71 <nickthorpe71@gmail.com>
2020-06-23 11:09:09 -0500

    Route added to index and app/ data added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
     </main>
   );
 }
+
+
 
 export default App;
