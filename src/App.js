020568d1888749e677045e642e3d6377ae95fd01
nickthorpe71 <nickthorpe71@gmail.com>
2020-06-23 11:40:31 -0500

    requirements added and components list created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import dummyData from './data';
 
-function App() {
-  return (
-    <main className='App'>
-    </main>
-  );
+class App extends React.Component() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: dummyData
+    };
+  }
+
+  render() {
+    return (
+      <main className='App'>
+      </main>
+    );
+  }
 }
-
-
 
 export default App;
